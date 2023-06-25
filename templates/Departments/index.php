1822c28fa7c616d3329a68dee4a9383b6147d18e
Hawkeye <swarup@agpaytech.co.uk>
2023-06-25 23:04:49 +0530

    department index page added

---
+++ templates/Departments/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @var App\View\AppView $this
+ */
+
+$this->Html->css([
+    "/lib/semantic-ui/dropdown/dropdown.min",
+    "/lib/semantic-ui/transition/transition.min",
+], ["block" => true]);
+?>
+
+
